1e444b8ebf1c666e88f8063c47021a5f81b73185
fjs72 <727558285@qq.com>
2019-12-23 20:48:48 +0800

    2.4.1配置项目内属性

--- src/main/java/com/book/code/controller/HelloWorldController.java
+++ src/main/java/com/book/code/controller/HelloWorldController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController   //返回默认结果为字符串
 public class HelloWorldController {
 
-    @RequestMapping("/hellop") //映射信息，往往是URL的组成部分
-    public String hello(){
+    @RequestMapping("/hello") //映射信息，往往是URL的组成部分
+    public String hello() {
         return "Hello World";
     }
 }
