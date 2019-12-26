a177ff13e1b272452e3be290a32beb184afd1f6e
fjs72 <727558285@qq.com>
2019-12-26 22:39:34 +0800

    4.8.1创建类MyServlet1 http://localhost:8888/myServlet

--- src/main/java/com/book/code/SpringbootHelloworldsApplication.java
+++ src/main/java/com/book/code/SpringbootHelloworldsApplication.java
@@ -1,11 +1,18 @@
 package com.book.code;
 
+import com.book.code.servlet.MyServlet1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootHelloworldsApplication {
 
+    @Bean
+   public ServletRegistrationBean MyServlet1(){
+       return new ServletRegistrationBean(new MyServlet1(),"/myServlet/*");
+   }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHelloworldsApplication.class, args);
     }
