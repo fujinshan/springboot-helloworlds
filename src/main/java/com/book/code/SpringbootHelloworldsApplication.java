83f117a5e0db83e11d6d3806561790cff8cae7e3
fjs72 <727558285@qq.com>
2019-12-26 22:43:32 +0800

    4.8.1创建类MyServlet2 http://localhost:8888/myServlet2
    前者是用@Bean注解注入Servlet类，而后者采用@ServletComponentScan注解自动扫描Servlet类

--- src/main/java/com/book/code/SpringbootHelloworldsApplication.java
+++ src/main/java/com/book/code/SpringbootHelloworldsApplication.java
@@ -3,16 +3,18 @@ package com.book.code;
 import com.book.code.servlet.MyServlet1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringbootHelloworldsApplication {
 
-    @Bean
+   /* @Bean
    public ServletRegistrationBean MyServlet1(){
        return new ServletRegistrationBean(new MyServlet1(),"/myServlet/*");
-   }
+   }*/
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHelloworldsApplication.class, args);
     }
